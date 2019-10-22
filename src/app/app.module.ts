5789011d1e72d8aec1e813351e8e724756f2d898
Olga Sulymenko <olgasulymenko@gmail.com>
2019-10-22 10:12:13 +0300

    material inputs for text and date

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CreateTripPageComponent } from './create-trip-page/create-trip-page.component';
 import { AgmCoreModule} from '@agm/core';
 import { MapComponent } from './map/map.component';
+import {MatNativeDateModule} from '@angular/material';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import {MatInputModule} from "@angular/material/input";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 @NgModule({
   declarations: [
     AppComponent,
     MainPageComponent,
     CreateTripPageComponent,
-    MapComponent
+    MapComponent,
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
+    MatDatepickerModule,
+    MatInputModule,
+    MatNativeDateModule,
+    ReactiveFormsModule,
     AgmCoreModule.forRoot({apiKey: 'AIzaSyBNlwQE0tQLMQbsUEvf-KRc1gxzP6-KXsQ'}),
   ],
   providers: [],
--- src/app/create-trip-page/create-trip-page.component.ts
+++ src/app/create-trip-page/create-trip-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Trip} from '../trip';
+import {FormControl} from "@angular/forms";
 
 @Component({
   selector: 'app-create-trip-page',
@@ -9,13 +10,17 @@ import {Trip} from '../trip';
 export class CreateTripPageComponent implements OnInit {
 
 
+  TripName = new FormControl();
+  TripDescription = new FormControl();
+  StartDate = new FormControl((new Date()).toISOString());
+  EndDate = new FormControl((new Date()).toISOString());
 
   trip: Trip = {
     id: 1,
     name: 'Mountains',
     startDate: '11/09/2019',
     endDate: '13/09/2019',
-    description: 'Highly recommended'
+    description: 'No dogs allowed'
   };
   constructor(
       ) { }
@@ -23,7 +28,11 @@ export class CreateTripPageComponent implements OnInit {
   create_trip()
   {
     alert("Not implemented yet");
-    console.log(this.trip);
+    console.log(this.TripName.value);
+    console.log(this.StartDate.value);
+    console.log(this.EndDate.value);
+    console.log(this.TripDescription.value);
+
   }
   ngOnInit() {
   }
