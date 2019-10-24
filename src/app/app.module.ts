feabf7d55c28f23ce27b38ddcd9086c0c6ed4dfc
Alex Shmidt <kav993@gmail.com>
2019-10-24 16:08:37 +0300

    refector material import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { AgmCoreModule} from '@agm/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MaterialModule} from './material.module'
+import { LoginPopUpComponent } from './login-pop-up/login-pop-up.component';
+import { RegisterPopUpComponent } from './register-pop-up/register-pop-up.component';
+import { MapComponent } from './map/map.component';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CreateTripPageComponent } from './create-trip-page/create-trip-page.component';
-import { AgmCoreModule} from '@agm/core';
-import { MapComponent } from './map/map.component';
-import {MatNativeDateModule} from '@angular/material';
-import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from "@angular/material/input";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatGridListModule} from "@angular/material/grid-list";
-import {MatIconModule} from "@angular/material/icon";
-import {MatListModule} from "@angular/material/list";
-import {MatButtonModule} from "@angular/material/button";
-import {MatButtonToggleModule} from "@angular/material/button-toggle";
 
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LoginPopUpComponent } from './login-pop-up/login-pop-up.component';
-import { RegisterPopUpComponent } from './register-pop-up/register-pop-up.component';
-
-import { MatButtonModule, MatDialogModule, MatFormFieldModule, MatInputModule, MatIconModule } from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -36,27 +25,15 @@ import { MatButtonModule, MatDialogModule, MatFormFieldModule, MatInputModule, M
     RegisterPopUpComponent,
   ],
   imports: [
+    MaterialModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
-    MatDatepickerModule,
-    MatInputModule,
-    MatGridListModule,
-    MatIconModule,
-    MatListModule,
-    MatButtonModule,
-    MatButtonToggleModule,
-    MatNativeDateModule,
     ReactiveFormsModule,
     AgmCoreModule.forRoot({apiKey:'AIzaSyBNlwQE0tQLMQbsUEvf-KRc1gxzP6-KXsQ'}),
     FormsModule,
     ReactiveFormsModule,
-    MatButtonModule,
-    MatDialogModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatIconModule,
   ],
   entryComponents: [
     LoginPopUpComponent,
