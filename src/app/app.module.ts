ff753904fcaf66d9ce0ce61e6d94a961ca1192b6
Mazzart <am.magicsmile@gmail.com>
2019-10-25 09:48:18 +0300

    Move footer into a separate component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { CreateTripPageComponent } from './create-trip-page/create-trip-page.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 @NgModule({
@@ -25,6 +26,7 @@ import { HeaderComponent } from './header/header.component';
     LoginPopUpComponent,
     RegisterPopUpComponent,
     HeaderComponent,
+    FooterComponent,
   ],
   imports: [
     MaterialModule,
