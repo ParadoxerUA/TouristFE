50be98e9a2ea596b1ac25ab150437f2d25f4b517
Alex Shmidt <kav993@gmail.com>
2019-10-25 14:55:41 +0300

    move trip.service in _service/

--- src/app/create-trip-page/create-trip-page.component.ts
+++ src/app/create-trip-page/create-trip-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Trip} from '../trip';
 import {FormControl} from "@angular/forms";
 import {generate} from "rxjs";
-import {TripService} from "../trip.service";
+import {TripService} from "../_services/trip.service";
 
 @Component({
   selector: 'app-create-trip-page',
