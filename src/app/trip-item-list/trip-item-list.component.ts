20bb8782140abe15be5c5d20d5d76b9109699688
Bogdan Lomako <lomakojob@gmail.com>
2019-10-30 13:54:00 +0000

    Added trip-page component structure

---
+++ src/app/trip-item-list/trip-item-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-item-list',
+  templateUrl: './trip-item-list.component.html',
+  styleUrls: ['./trip-item-list.component.css']
+})
+export class TripItemListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/trip-user-list/trip-user-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-trip-user-list',
+  templateUrl: './trip-user-list.component.html',
+  styleUrls: ['./trip-user-list.component.css']
+})
+export class TripUserListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
