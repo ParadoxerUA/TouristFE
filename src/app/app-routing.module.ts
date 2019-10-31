a60bd6eb57ceb8daa4e0043277cdf1db32f8e227
Mazzart <am.magicsmile@gmail.com>
2019-10-31 09:58:16 +0200

    Add path to the trip_detail page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {MainPageComponent} from "./main-page/main-page.component";
 import {CreateTripPageComponent} from "./create-trip-page/create-trip-page.component";
-import {EmailConfirmationComponent} from './email-confirmation/email-confirmation.component'
+import {EmailConfirmationComponent} from './email-confirmation/email-confirmation.component';
+import {TripDetailPageComponent} from './trip-detail-page/trip-detail-page.component';
 
 const routes: Routes = [
   { path: 'index', component: MainPageComponent },
   { path: '', redirectTo: '/index', pathMatch: 'full' },
   { path: 'create_trip', component: CreateTripPageComponent },
-  {path: 'email-confirmation', component: EmailConfirmationComponent}
+  { path: 'email-confirmation', component: EmailConfirmationComponent },
+  { path: 'trip_detail', component: TripDetailPageComponent }
 ];
 
 @NgModule({
