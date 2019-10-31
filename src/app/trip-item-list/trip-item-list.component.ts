668d9f88834932cdf4a1c32a2a056b3d1800aabb
Mazzart <am.magicsmile@gmail.com>
2019-10-31 15:48:35 +0200

    Input form to add item

--- src/app/trip-item-list/trip-item-list.component.ts
+++ src/app/trip-item-list/trip-item-list.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TripItemListComponent implements OnInit {
 
+  name: string;
+  weight: number;
+  amount: number;
+  items = [];
+
+  addItem() {
+    this.items.push({name: this.name,
+                     weight: this.weight,
+                     amount: this.amount});
+    this.name = '';
+    this.weight = 0;
+    this.amount = 0;
+  }
+
   constructor() { }
 
   ngOnInit() {
