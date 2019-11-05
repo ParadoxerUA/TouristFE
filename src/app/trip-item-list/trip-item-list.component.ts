18c43447d3cb82b4261b03b37329e6fea5983331
Mazzart <am.magicsmile@gmail.com>
2019-11-05 13:22:17 +0200

    Fix

--- src/app/trip-item-list/trip-item-list.component.ts
+++ src/app/trip-item-list/trip-item-list.component.ts
@@ -1,12 +1,7 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
-
-export interface Items {
-  name: string;
-  weight: number;
-  amount: number;
-}
+import { Items } from '../trip';
 
 const ITEMS: Items[] = [];
 
