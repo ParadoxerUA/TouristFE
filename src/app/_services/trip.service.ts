f4a2effd0d91d46dcb3643d7a608f03849332afc
Olga Sulymenko <olgasulymenko@gmail.com>
2019-11-13 16:09:38 +0000

    Feature/tour 8 map expand menu

--- src/app/_services/trip.service.ts
+++ src/app/_services/trip.service.ts
@@ -66,7 +66,7 @@ export class TripService {
 
   getTrip(trip_id: number): Observable<any> {
     let header = new HttpHeaders({'Authorization': this.userService.getSessionId()});
-    const url = `${this.tripUrl}/${trip_id}?fields=name,start_date,description,end_date,points,trip_uuid,trip_id,users,admin_id`;
+    const url = `${this.tripUrl}/${trip_id}`;
     return this.http.get(url, {headers: header})
   }
 
@@ -93,4 +93,10 @@ export class TripService {
     };
     return this.http.patch(updateTripUrl, trip, this.httpOptions);
   }
+
+    refreshInviteLink(trip_id: number): Observable<any>  {
+        const tripRefreshUrl: string = `${BASE_URL}/trip/v1/manage_trip/${trip_id}`;
+        return this.http.patch(tripRefreshUrl, null,  {
+            headers: {'Authorization': this.userService.getSessionId()}, observe: 'response'})
+    }
 }
