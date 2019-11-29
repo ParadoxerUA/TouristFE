1be98ea2f08c5bf905bab090736b90141f88c541
Kirill Redin <redinkirill.ua@gmail.com>
2019-11-29 16:56:01 +0200

    Fix change password, login, logout and change capacity urls.

--- src/app/user-profile/user-profile.component.ts
+++ src/app/user-profile/user-profile.component.ts
@@ -20,7 +20,6 @@ export class UserProfileComponent implements OnInit {
   calculateFormOpened = false;
   passwordFormOpened = false;
   result: number;
-  gender: string;
 
   userGender = new FormControl('', Validators.required);
   userHeight = this.getUserFormControl();
@@ -50,11 +49,11 @@ export class UserProfileComponent implements OnInit {
  }
 
   changeCapacity(): void {
-    let delta = this.gender === "male" ? 4 : 0;
+    let delta = this.userGender.value === "male" ? 4 : 0;
 
     this.result = ((this.userWeight.value * 0.3) + ((this.userHeight.value-100)/5) + delta) / 2;
 
-    this.userService.updateCapacity(this.result)
+    this.userService.updateCapacity({'capacity': this.result})
     .subscribe(() => this.userService.refreshUser());
     this.calculateFormOpened = false;
   }
